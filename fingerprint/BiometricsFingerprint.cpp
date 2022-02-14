861bf83c802aa4854a556fc962dd97ac49906462
Dil3mm4 <dil3mm4.dev@gmail.com>
2022-02-14 02:39:54 +0100

    fixup! 68e23552961b4be108dfb477d10ab1be811ab699: reinherit set for pressed path

--- fingerprint/BiometricsFingerprint.cpp
+++ fingerprint/BiometricsFingerprint.cpp
@@ -472,11 +472,13 @@ Return<bool> BiometricsFingerprint::isUdfps(uint32_t /* sensorId */) {
 Return<void> BiometricsFingerprint::onFingerDown(uint32_t /* x */, uint32_t /* y */,
                                                 float /* minor */, float /* major */) {
     set(FOD_STATUS_PATH, FOD_STATUS_ON);
+    set(FOD_PRESSED_PATH, FOD_PRESSED_ON);
     return Void();
 }
 
 Return<void> BiometricsFingerprint::onFingerUp() {
     set(FOD_STATUS_PATH, FOD_STATUS_OFF);
+    set(FOD_PRESSED_PATH, FOD_PRESSED_OFF);
     return Void();
 }
 
